a78baade7a518c4d0ad7608cd9aefd05db13421b
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-04-22 11:26:48 -0300

    Added model Movie

---
+++ MayTheFourthApi/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace MayTheFourthApi.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string episode { get; set; }
+        public string OpeningCrawl { get; set; }
+        public string Director { get; set; }
+        public string Producer { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+        public List<Character> Characters { get; set; }
+        public List<Planet> Planets { get; set; }
+        public List<Vehicle> Vehicles { get; set; }
+        public List<Starship> Starships { get; set; }
+    }
+}
