de1c4ddc1aaaf3886e1d4bb1fe42fa55af4f1326
Matheus Andre <matheusandr2@gmail.com>
2024-04-22 11:49:31 -0300

    Adicionando Classes Character e StarShip

---
+++ MayTheFourthApi/Models/Character.cs
@@ -0,0 +1,18 @@
+﻿namespace MayTheFourthApi.Models;
+
+public class Character
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Height { get; set; } = string.Empty;
+    public string Weigth { get; set; } = string.Empty;
+    public string HairColor { get; set; } = string.Empty;
+    public string SkinColor { get; set; } = string.Empty;
+    public string EyeColor { get; set; } = string.Empty;
+    public string BirthYear { get; set; } = string.Empty;
+    public string Gender { get; set; } = string.Empty;
+    public List<Planet> Planets {  get; set; } 
+    public List<Movie> Movies { get; set; }
+
+
+}
---
+++ MayTheFourthApi/Models/StarShip.cs
@@ -0,0 +1,21 @@
+﻿namespace MayTheFourthApi.Models;
+
+public class StarShip
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Manufacturer { get; set; } = string.Empty;
+    public string CostInCredits { get; set; } = string.Empty;
+    public string Length {  get; set; } = string.Empty;
+    public string MaxSpeed {  get; set; } = string.Empty;
+    public string Crew { get; set; } = string.Empty;
+    public string Passengers {  get; set; } = string.Empty;
+    public string CargoCapacity { get; set; } = string.Empty;
+    public string HyperDriveRating {  get; set; } = string.Empty;
+
+    public string Mglt { get; set; } = string.Empty;
+    public string Consumables { get; set; } = string.Empty;
+    public string Class { get; set; } = string.Empty;
+    public List<Movie> Movies { get; set; }
+
+}
