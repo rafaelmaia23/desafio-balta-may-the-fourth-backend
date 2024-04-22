0a3dfc22ee54bf049ed67498fe84b9dd564fbb02
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-04-22 11:16:42 -0300

    Added DbContext in program.cs

--- MayTheFourthApi/Data/AppDbContext.cs
+++ MayTheFourthApi/Data/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace MayTheFourthApi.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MayTheFourthApi.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
     }
 }
