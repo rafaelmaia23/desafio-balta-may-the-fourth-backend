543ecf12633f6ca6dbfb3704cabdd6a56290154a
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-04-22 11:35:24 -0300

    Added model Planet, added id to movie model

--- MayTheFourthApi/Models/Movie.cs
+++ MayTheFourthApi/Models/Movie.cs
@@ -1,16 +1,19 @@
-﻿namespace MayTheFourthApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MayTheFourthApi.Models;
+
+public class Movie
 {
-    public class Movie
-    {
-        public string Title { get; set; }
-        public string episode { get; set; }
-        public string OpeningCrawl { get; set; }
-        public string Director { get; set; }
-        public string Producer { get; set; }
-        public DateOnly ReleaseDate { get; set; }
-        public List<Character> Characters { get; set; }
-        public List<Planet> Planets { get; set; }
-        public List<Vehicle> Vehicles { get; set; }
-        public List<Starship> Starships { get; set; }
-    }
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string episode { get; set; }
+    public string OpeningCrawl { get; set; }
+    public string Director { get; set; }
+    public string Producer { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+    public List<Character> Characters { get; set; }
+    public List<Planet> Planets { get; set; }
+    public List<Vehicle> Vehicles { get; set; }
+    public List<Starship> Starships { get; set; }
 }
