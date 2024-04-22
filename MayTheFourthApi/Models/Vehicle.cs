5022d6360f9544fbf5dde6ac6f3e01264de92620
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-04-22 11:39:25 -0300

    Added model Vehicle

---
+++ MayTheFourthApi/Models/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MayTheFourthApi.Models;
+
+public class Vehicle
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public string CostInCredits { get; set; }
+    public string Length { get; set; }
+    public string MaxSpeed { get; set; }
+    public string Crew { get; set; }
+    public string Passengers { get; set; }
+    public string CargoCapacity { get; set; }
+    public string Consumables { get; set; }
+    public string Class { get; set; }
+    public List<Movie> Movies { get; set; }
+}
