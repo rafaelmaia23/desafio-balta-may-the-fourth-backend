cd131b89332ccf18acc5430d19966da72be119ff
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-04-24 08:07:28 -0300

    Implementado Iservice em PlanetService

--- MayTheFourthApi/Services/PlanetService.cs
+++ MayTheFourthApi/Services/PlanetService.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MayTheFourthApi.Services
 {
-    public class PlanetService : IPlanetService
+    public class PlanetService : IService<Planet>
     {
         private AppDbContext _db;
 
@@ -14,33 +14,23 @@ namespace MayTheFourthApi.Services
             _db = db;
         }
 
-        public async Task AddPlanet(Planet planet)
+        public async Task AddRegister(Planet planet)
         {
             await _db.Planets.AddAsync(planet);
             await _db.SaveChangesAsync();
         }
 
-        public async Task<bool> DeletePlanet(int id)
-        {
-            var planet = await _db.Planets.FirstOrDefaultAsync(x => x.Id == id);
-            if (planet == null) return false;
-
-            _db.Planets.Remove(planet);
-            await _db.SaveChangesAsync();
-            return true;
-        }
-
-        public async Task<Planet?> GetPlanetById(int id)
-        {
-            return await _db.Planets.FirstOrDefaultAsync(x => x.Id == id);
-        }
-
-        public async Task<IEnumerable<Planet>> GetPlanets()
+        public async Task<IEnumerable<Planet>> GetRegisters()
         {
             return _db.Planets;
         }
 
-        public async Task<Planet> UpdatePlanet(int id, Planet planet)
+        public async Task<Planet?> GetRegisterById(int id)
+        {
+            return await _db.Planets.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Planet> UpdateRegister(int id, Planet planet)
         {
             var planetToBeUpdated = await _db.Planets.FirstOrDefaultAsync(x => x.Id == id);
             if (planetToBeUpdated == null) return null;
@@ -58,6 +48,17 @@ namespace MayTheFourthApi.Services
             _db.Planets.Update(planetToBeUpdated);
             await _db.SaveChangesAsync();
             return planetToBeUpdated;
+        }
+
+        public async Task<bool> DeleteRegister(int id)
+        {
+            var planet = await _db.Planets.FirstOrDefaultAsync(x => x.Id == id);
+            if (planet == null) return false;
+
+            _db.Planets.Remove(planet);
+            await _db.SaveChangesAsync();
+
+            return true;
         }
     }
 }
